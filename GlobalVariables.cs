109d568e141fbd4ef2dfa5edacae13fc61732915
rykos <rykos.net@gmail.com>
2020-06-14 16:38:43 +0200

    Refactored code

---
+++ GlobalVariables.cs
@@ -0,0 +1,12 @@
+namespace GlobalVariables
+{
+    public static class Variables
+    {
+        public const string AppDataNode = "//div[@class='hAyfc']";
+        public const string AppDataCell = ".//div[@class='BgcNfc']";
+        public const string AppDataCellCenter = ".//div[@class='IQ1z0d']//span";
+        public const string AppName = "//h1[@class='AHFaub']";
+        public const string AppRating = "//div[@class='BHMmbe']";
+        public const string AppRatingsAmount = "//span[@class='EymY4b']";
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,58 +5,85 @@ using System.Linq;
 using System.Text.Json;
 using System.Text.RegularExpressions;
 using HtmlAgilityPack;
+using GlobalVariables;
 
-namespace webScraper {
-    class Program {
-        static readonly string[] reqs = LoadReq ();
+namespace webScraper
+{
+    class Program
+    {
+        static readonly string[] Requirements = LoadRequirements();
 
-        static void Main (string[] args) {
-            string[] filePaths = Directory.GetFiles ("sites");
-            List<Dictionary<string, string>> nodesDataList = new List<Dictionary<string, string>> ();
-            foreach (string filePath in filePaths) {
-                try {
-                    Dictionary<string, string> skimmedWebsite = SkimWebsite (filePath);
-                    if (skimmedWebsite.Keys.Count < reqs.Count ()) {
-                        throw new System.Exception ();
-                    }
-                    nodesDataList.Add (skimmedWebsite);
-                } catch {
-                    Console.WriteLine ("Failed to skimm at: " + filePath);
-                }
+        static void Main(string[] args)
+        {
+            string[] filePaths = Directory.GetFiles("sites");
+            if (filePaths.Count() < 1)
+            {
+                throw new System.Exception("There are no sites to scrap in 'sites' folder at root directory");
             }
-            string json = JsonSerializer.Serialize (nodesDataList, new JsonSerializerOptions () { WriteIndented = true });
-            File.WriteAllText ("JsonData.json", json);
-        }
-
-        private static Dictionary<string, string> SkimWebsite (string path) {
-            Dictionary<string, string> nodesData = new Dictionary<string, string> ();
-            HtmlDocument doc = new HtmlDocument ();
-            doc.Load (path);
-            var masterNodes = doc.DocumentNode.SelectNodes ("//div[@class='hAyfc']");
-            foreach (var n in masterNodes) {
-                var node = n.SelectSingleNode (".//div[@class='BgcNfc']");
-                if (reqs.Contains (node.InnerText)) //Name node
+            List<Dictionary<string, string>> nodesDataList = new List<Dictionary<string, string>>();
+            foreach (string filePath in filePaths)
+            {
+                try
                 {
-                    string nodeName = node.InnerText;
-                    var informationNode = n.SelectSingleNode (".//div[@class='IQ1z0d']//span");
-                    Regex regex = new Regex ("[ ]{2,}|\\n|\\u002B", RegexOptions.None);
-                    string informationNodeText = regex.Replace (informationNode.InnerText, " ");
-                    nodesData.Add (node.InnerText, informationNodeText); //Add result to data
+                    Dictionary<string, string> skimmedWebsite = ScrapWebsiteAt(filePath);
+                    if (skimmedWebsite.Keys.Count < Requirements.Count())
+                    {
+                        throw new System.Exception();
+                    }
+                    nodesDataList.Add(skimmedWebsite);
+                }
+                catch
+                {
+                    Console.WriteLine("Failed to scrap site: " + filePath);
                 }
             }
-            Regex localRegex = new Regex ("[ ]{2,}|\\n|\\u002B", RegexOptions.None);
-            nodesData.Add ("Rating", doc.DocumentNode.SelectSingleNode ("//div[@class='BHMmbe']").InnerText);
-            nodesData.Add ("Ratings", doc.DocumentNode.SelectSingleNode ("//span[@class='EymY4b']").InnerText);
-            string modifiedString = localRegex.Replace (doc.DocumentNode.SelectSingleNode ("//h1[@class='AHFaub']").InnerText, " ");
-            nodesData.Add ("Name", modifiedString);
-            return nodesData;
+            string json = JsonSerializer.Serialize(nodesDataList, new JsonSerializerOptions() { WriteIndented = true });
+            File.WriteAllText("JsonData.json", json);
         }
 
-        private static string[] LoadReq () {
-            if (!File.Exists ("Requirements")) {
-                throw new System.Exception ("Requirements file does not exist in root directory");
+        private static Dictionary<string, string> ScrapWebsiteAt(string path)
+        {
+            Dictionary<string, string> scrappedData = new Dictionary<string, string>();
+            HtmlDocument doc = new HtmlDocument();
+            doc.Load(path);
+            HtmlNodeCollection appDataNodes = doc.DocumentNode.SelectNodes(Variables.AppDataNode);
+            foreach (HtmlNode node in appDataNodes)
+            {
+                HtmlNode currentNode = node.SelectSingleNode(Variables.AppDataCell);
+                string currentNodeName = currentNode.InnerText;
+                if (Requirements.Contains(currentNodeName))//Data we are looking for
+                {
+                    HtmlNode informationNode = node.SelectSingleNode(Variables.AppDataCellCenter);
+                    string informationNodeText = FormatScrapedString(informationNode.InnerText);
+                    scrappedData.Add(currentNodeName, informationNodeText); //Add result to data
+                }
             }
-            string[] reqs = File.ReadAllLines ("Requirements");
+            scrappedData.Add("Rating", doc.DocumentNode.SelectSingleNode(Variables.AppRating).InnerText);
+            scrappedData.Add("Ratings", doc.DocumentNode.SelectSingleNode(Variables.AppRatingsAmount).InnerText);
+            scrappedData.Add("Name", FormatScrapedString(doc.DocumentNode.SelectSingleNode(Variables.AppName).InnerText));
+            return scrappedData;
+        }
+
+        private static string FormatScrapedString(string input)
+        {
+            Regex regex = new Regex("[ ]{2,}|\\n|\\u002B", RegexOptions.None);
+            return regex.Replace(input, " "); ;
+        }
+
+        private static string[] LoadRequirements()
+        {
+            if (!File.Exists("Requirements"))
+            {
+                throw new System.Exception("Requirements file does not exist in root directory");
+            }
+            string[] reqs = File.ReadAllLines("Requirements").Where(line =>
+            {
+                return (line[0] == '#') ? false : true;
+            }).ToArray();
+            if (reqs.Count() < 1)
+            {
+                throw new System.Exception("There are no information specified to gather");
+            }
             return reqs;
         }
     }
